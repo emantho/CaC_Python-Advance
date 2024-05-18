d4d4a05b0a944b967c9caaf54f7637b4a3a83264
Eder Manjarres <emanja1014@gmail.com>
2024-05-17 23:36:34 -0300

    + Added two new web pages + adde ststic folders and files + Created base and index css + added logo

--- myCoffeeShop/alba_pasticceria/urls.py
+++ myCoffeeShop/alba_pasticceria/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('sayHello/<str:userName>', views.sayHello, name='sayHello'),
+    path('listName', views.listName, name='nameList'),
 ]
--- myCoffeeShop/alba_pasticceria/views.py
+++ myCoffeeShop/alba_pasticceria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
@@ -7,5 +8,28 @@ from django.http import HttpResponse
 #     return HttpResponse("Hello to ALBA PASTICCERIA")
 
 def index(request):
-    return render(request, 'alba_pasticceria/index.html')
+    
+    my_context_var = {
+        'userName':'Diana',
+        'dateTime': datetime.datetime.now()
+    }
+    
+    return render(request, 'alba_pasticceria/index.html', my_context_var)
+
+def sayHello(request, userName):
+    return HttpResponse(("Hello World {} from Django, Welcome ".format(userName)))
+
+def listName(request):
+    
+    my_context = {
+        'names' : [
+            "Paola Moreno",
+            "Laura Fernandez",
+            "Emilia Manjarres"
+        ], 
+        'payment_status': True
+        
+    }
+    
+    return render(request, 'alba_pasticceria/nameList.html', my_context)
 
